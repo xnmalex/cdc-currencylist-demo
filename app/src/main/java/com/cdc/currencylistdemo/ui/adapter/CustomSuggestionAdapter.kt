03819730fc2fb9f465d77c24d5f8aeca6398f088
Alex Gui <xnmalex@gmail.com>
2025-07-17 02:45:03 +0800

    fixed custom suggestion to return all data from viewmodel without filtering

--- app/src/main/java/com/cdc/currencylistdemo/ui/adapter/CustomSuggestionAdapter.kt
+++ app/src/main/java/com/cdc/currencylistdemo/ui/adapter/CustomSuggestionAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Filter
 import android.widget.TextView
 import com.cdc.currencylistdemo.R
 import com.cdc.currencylistdemo.domain.model.CurrencyInfo
@@ -12,6 +13,8 @@ import com.cdc.currencylistdemo.domain.model.CurrencyInfo
 class CustomSuggestionAdapter(
     context: Context,
 ) : ArrayAdapter<CurrencyInfo>(context, 0, mutableListOf()) {
+
+    private val list = mutableListOf<CurrencyInfo>()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val suggestion = getItem(position)
@@ -28,11 +31,45 @@ class CustomSuggestionAdapter(
         nameText.text = suggestion?.name ?: ""
         symbolText.text = suggestion?.symbol ?: ""
 
+        symbolText.visibility = if(suggestion?.type == "crypto") View.VISIBLE else  View.GONE
+
         return view
+    }
+
+    override fun getItem(position: Int): CurrencyInfo? {
+        return super.getItem(position)
+    }
+
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val results = FilterResults()
+                val suggestions = mutableListOf<CurrencyInfo>()
+
+                // Always return all items from the adapter
+                suggestions.addAll(list)
+
+                results.values = suggestions
+                results.count = suggestions.size
+                return results
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                clear()
+                if (results != null && results.count > 0) {
+                    @Suppress("UNCHECKED_CAST")
+                    addAll(results.values as List<CurrencyInfo>)
+                }
+                notifyDataSetChanged()
+            }
+        }
     }
 
     fun updateItems(newItems: List<CurrencyInfo>) {
         clear()
+        list.clear()
+        list.addAll(newItems)
         addAll(newItems)
         notifyDataSetChanged()
     }
